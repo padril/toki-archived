496af04831be384ba6d430f8d9071b3c65388ccb
padri <padrillium@gmail.com>
2023-11-04 09:26:02 -0400

    Used pylint to get consistent style in lint.py

--- lint.py
+++ lint.py
@@ -1,17 +1,30 @@
 # Copyright 2023 Leo James Peckham (padril)
 
-# Lints `toki.c`
+"""
+Lints `toki.c` according to cppcheck's standards, and in the future, will check
+for consistency in variable names.
 
-import shlex, subprocess
+If running this doesn't work, make sure you have `cppcheck` installed and in
+your PATH environment variable
+"""
 
-# If running this doesn't work, make sure you have `cppcheck` installed
-# and in your PATH environment variable
 
-if __name__ == "__main__":
+import shlex
+import subprocess
+
+
+def lint():
+    """
+    Runs lint process
+    """
     command = "cppcheck "\
               "--enable=all "\
               "--force "\
               "--suppress=missingIncludeSystem "\
               "toki.c"
     split = shlex.split(command)
-    subprocess.run(split)
+    subprocess.run(split, check=True)
+
+
+if __name__ == "__main__":
+    lint()
